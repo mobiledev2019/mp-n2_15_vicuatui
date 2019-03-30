a04f717dbd1d2113c8729b1bd120f21010a140ee
huyhoquang <huy@passcareer.io>
2019-03-30 23:31:27 +0700

    add navigation bottom to app

--- app/src/main/java/com/example/vicuatui/MainActivity.java
+++ app/src/main/java/com/example/vicuatui/MainActivity.java
@@ -1,17 +1,73 @@
 package com.example.vicuatui;
 
+import android.support.annotation.NonNull;
+import android.support.design.internal.BottomNavigationMenuView;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.DialogFragment;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView mMainNav;
+    private FrameLayout mMainFrame;
+
+    private FirstFragment firstFragment;
+    private SecondFragment secondFragment;
+    private ThirdFragment thirdFragment;
+    private FourthFragment fourthFragment;
+    private FifthFragment fifthFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMainFrame = (FrameLayout) findViewById(R.id.main_frame);
+        mMainNav = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        firstFragment = new FirstFragment();
+        secondFragment = new SecondFragment();
+        thirdFragment = new ThirdFragment();
+        fourthFragment = new FourthFragment();
+        fifthFragment = new FifthFragment();
+
+        mMainNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_first:
+                        serFregment(firstFragment);
+                        return true;
+                    case R.id.nav_second:
+                        serFregment(secondFragment);
+                        return true;
+                    case R.id.nav_third:
+                        serFregment(thirdFragment);
+                        return true;
+                    case R.id.nav_fourth:
+                        serFregment(fourthFragment);
+                        return true;
+                    case R.id.nav_fifth:
+                        serFregment(fifthFragment);
+                        return true;
+                        default:
+                            return false;
+                }
+            }
+        });
+    }
+
+    private void serFregment(Fragment fragment) {
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_frame, fragment);
+        fragmentTransaction.commit();
     }
 
     public void showDatePicker(View view) {
