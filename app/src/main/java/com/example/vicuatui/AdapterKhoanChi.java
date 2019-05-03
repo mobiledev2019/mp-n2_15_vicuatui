3a4fa0f51a88fb95b674f1d32fc0fe60d750df19
huyhoquang <huy@passcareer.io>
2019-05-03 15:13:07 +0700

    read data from database to second page

---
+++ app/src/main/java/com/example/vicuatui/AdapterKhoanChi.java
@@ -0,0 +1,76 @@
+package com.example.vicuatui;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class AdapterKhoanChi extends BaseAdapter {
+    Activity context;
+    ArrayList list;
+
+    public AdapterKhoanChi(Activity context, ArrayList list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View row = inflater.inflate(R.layout.listview_row, null);
+        ImageView img_hang_muc = (ImageView) row.findViewById(R.id.img_hang_muc);
+        TextView txt_hang_muc = (TextView) row.findViewById(R.id.txt_hang_muc);
+        TextView txt_dien_giai = (TextView) row.findViewById(R.id.txt_dien_giai);
+        TextView txt_ngay_thang = (TextView) row.findViewById(R.id.txt_ngay_thang);
+        TextView txt_so_tien = (TextView) row.findViewById(R.id.txt_so_tien);
+        Button btn_sua = (Button) row.findViewById(R.id.btn_sua);
+        Button btn_xoa = (Button) row.findViewById(R.id.btn_xoa);
+
+        final KhoanChi khoanChi = (KhoanChi) list.get(position);
+        txt_hang_muc.setText(khoanChi.hangMuc);
+        txt_dien_giai.setText(khoanChi.dienGiai);
+        txt_ngay_thang.setText(khoanChi.ngayThang);
+        txt_so_tien.setText(khoanChi.soTien + "");
+
+        Bitmap bmHinhDaiDien = BitmapFactory.decodeByteArray(khoanChi.anhHangMuc, 0 , khoanChi.anhHangMuc.length);
+        img_hang_muc.setImageBitmap(bmHinhDaiDien);
+
+//        btn_sua.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(context, UpdateActivity.class);
+//                intent.putExtra("ID", khoanChi.ID);
+//                context.startActivity(intent);
+//            }
+//        });
+        return row;
+    }
+
+
+}
--- app/src/main/java/com/example/vicuatui/SecondFragment.java
+++ app/src/main/java/com/example/vicuatui/SecondFragment.java
@@ -1,29 +1,69 @@
 package com.example.vicuatui;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import static android.graphics.Color.*;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class SecondFragment extends Fragment {
+    final String DATABASE_NAME = "VicuatuiBDv1.db";
+    SQLiteDatabase database;
 
+    ListView listView;
+    ArrayList<KhoanChi> list;
+    AdapterKhoanChi adapter;
 
     public SecondFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        addControl(view);
+        readData();
+        return view;
     }
 
+    public void addControl(View view) {
+        listView = view.findViewById(R.id.listview);
+        list = new ArrayList<>();
+        adapter = new AdapterKhoanChi(getActivity(), list);
+        listView.setAdapter(adapter);
+    }
+
+    private void readData() {
+        database = (SQLiteDatabase) Database.initDatabase(getActivity(), DATABASE_NAME);
+        Cursor cursor = database.rawQuery("SELECT * FROM KhoanChi", null);
+        list.clear();
+        for(int i=0; i<cursor.getCount(); i++) {
+            cursor.moveToPosition(i);
+            int id = cursor.getInt(0);
+            double soTien = cursor.getDouble(1);
+            String hangMuc = cursor.getString(2);
+            String dienGiai = cursor.getString(3);
+            String ngayThang = cursor.getString(4);
+            byte[] anhHangMuc = cursor.getBlob(5);
+            list.add(new KhoanChi(id, soTien, hangMuc, dienGiai, ngayThang, anhHangMuc));
+        }
+        adapter.notifyDataSetChanged();
+    }
 }
