2222eac6798f288c21013d30e59b48c69a4dd142
quanglv <quanglv@chainos.vn>
2019-05-22 09:33:40 +0700

    add validate

--- app/src/main/java/com/example/vicuatui/ThirdFragment.java
+++ app/src/main/java/com/example/vicuatui/ThirdFragment.java
@@ -14,8 +14,10 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +29,10 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
+import com.basgeekball.awesomevalidation.utility.RegexTemplate;
+
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -34,6 +40,8 @@ import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
+
+import static android.content.ContentValues.TAG;
 
 
 /**
@@ -52,6 +60,9 @@ public class ThirdFragment extends Fragment {
     ImageView imgBill;
     Spinner spinner_hang_muc;
     Button btnGhi, btnDatePicker;
+    private TextInputLayout inputLayoutDienDai, inputLayoutTien, inputLayoutNgayThang;
+    AwesomeValidation awesomeValidation;
+
 
     public ThirdFragment() {
         // Required empty public constructor
@@ -62,6 +73,8 @@ public class ThirdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_third, container, false);
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
         addControls(view);
         bundle = this.getArguments();
         editData();
@@ -80,11 +93,23 @@ public class ThirdFragment extends Fragment {
         contentValues.put("HangMuc", hangMuc);
         contentValues.put("DienGiai", dienGiai);
         contentValues.put("NgayThang", ngayThang);
-        contentValues.put("Anh", anh);
+        //contentValues.put("Anh", anh);
+        if (anh.length > 0 || anh != null){
+            Log.d(TAG , "LOG HERE2");
+            contentValues.put("Anh", anh);
+
+        }
+        else {
+            Log.d(TAG , "LOG HERE");
+        }
 
         SQLiteDatabase database = Database.initDatabase(getActivity(), "VicuatuiBDv1.db");
         database.insert("KhoanChi", null, contentValues);
         Toast.makeText(getActivity(), "Lưu thành công", Toast.LENGTH_SHORT).show();
+        txt_dien_giai.setText("");
+        txt_ngay_thang.setText("");
+        txt_so_tien.setText("");
+        imgBill.setImageBitmap(null);
     }
 
     //TODO: get  and update data in fragment
@@ -111,6 +136,7 @@ public class ThirdFragment extends Fragment {
     }
 
     private void updateData() {
+        Log.d(TAG , "LOG HERE");
         double soTien = (double) Double.parseDouble(txt_so_tien.getText() + "");
         String hangMuc = spinner_hang_muc.getSelectedItem().toString();
         String dienGiai = txt_dien_giai.getText().toString();
@@ -120,9 +146,17 @@ public class ThirdFragment extends Fragment {
         ContentValues contentValues = new ContentValues();
         contentValues.put("SoTien", soTien);
         contentValues.put("HangMuc", hangMuc);
-        contentValues.put("DienGiai", dienGiai);
+        //contentValues.put("DienGiai", dienGiai);
         contentValues.put("NgayThang", ngayThang);
-        contentValues.put("Anh", anh);
+        if (anh.length > 0 || anh != null){
+            Log.d(TAG , "LOG HERE2" + anh.length);
+            contentValues.put("Anh", anh);
+
+        }
+        else {
+            Log.d(TAG , "LOG HERE");
+        }
+
 
         SQLiteDatabase database = Database.initDatabase(getActivity(), "VicuatuiBDv1.db");
         database.update("KhoanChi", contentValues, "id = ?", new String[] { id + ""});
@@ -137,15 +171,34 @@ public class ThirdFragment extends Fragment {
         btnDatePicker = (Button) view.findViewById(R.id.third_fragment__btn_date_picker);
         txvAddBill = (TextView) view.findViewById(R.id.third_fragment__txv_add_bill);
         imgBill = (ImageView) view.findViewById(R.id.third_fragment__img_bill);
+        inputLayoutNgayThang = (TextInputLayout) view.findViewById(R.id.inputLayoutNgayThang);
+        inputLayoutTien = (TextInputLayout) view.findViewById(R.id.inputLayoutTien);
+        inputLayoutDienDai = (TextInputLayout) view.findViewById(R.id.inputLayoutDienGiai);
 
-        txt_so_tien.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(txt_so_tien.getText().length() < 5){
-                    txt_so_tien.setError("Must be 5 charater");
-                }
-            }
-        });
+
+//        txt_so_tien.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+////                if(txt_so_tien.getText().length() == 0){
+////                    txt_so_tien.setError("Không được để trống !");
+////                }
+//                if (txt_so_tien.getText().length() < 3 || txt_so_tien.getText().length() > 11){
+//                    txt_so_tien.setError("Số tiền không hợp lệ");
+//                }
+//            }
+//        });
+//
+//        txt_dien_giai.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if(txt_dien_giai.getText().length() == 0){
+//                    txt_dien_giai.setError("Không được để trống !");
+//                }
+//            }
+//        });
+
+
+
 
         Calendar c = Calendar.getInstance();
         mDay = c.get(Calendar.DATE);
@@ -157,16 +210,19 @@ public class ThirdFragment extends Fragment {
         btnGhi.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (bundle !=  null) {
-                    if (bundle.getString("Edit") == "true") {
-                        updateData();
-                        Toast.makeText(getActivity(), "Sửa thành công", Toast.LENGTH_SHORT).show();
-                    }
+                    if (validate()){
+                        if (bundle !=  null) {
+                            if (bundle.getString("Edit") == "true") {
+                                updateData();
+                                Toast.makeText(getActivity(), "Sửa thành công", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            saveData();
+                        }
+                            Toast.makeText(getActivity() , "Data receove success" , Toast.LENGTH_SHORT).show();
+                        }
                 }
-                else {
-                    saveData();
-                }
-            }
         });
 
         txvAddBill.setOnClickListener(new View.OnClickListener() {
@@ -175,6 +231,37 @@ public class ThirdFragment extends Fragment {
                 takePhoto();
             }
         });
+    }
+
+    private boolean validate() {
+
+        boolean isValid = true;
+
+        if (txt_dien_giai.getText().toString().isEmpty()) {
+            inputLayoutDienDai.setError("Không được để trống");
+            isValid = false;
+        } else {
+            inputLayoutDienDai.setErrorEnabled(false);
+        }
+
+        if (txt_so_tien.getText().toString().isEmpty() && txt_so_tien.getText().toString().trim().length() < 3 && txt_so_tien.getText().toString().trim().length() <12) {
+            inputLayoutTien.setError("Số tiền không hợp lệ");
+            isValid = false;
+        } else {
+            inputLayoutTien.setErrorEnabled(false);
+        }
+
+        if (txt_ngay_thang.getText().toString().trim().isEmpty() ) {
+            inputLayoutNgayThang.setError("Ngày tháng không hợp lệ");
+            isValid = false;
+        } else {
+            inputLayoutNgayThang.setErrorEnabled(false);
+        }
+
+        if (isValid) {
+            Toast.makeText(getActivity(), R.string.success, Toast.LENGTH_SHORT).show();
+        }
+        return isValid;
     }
 
     private int findItemSpinner(String string) {
